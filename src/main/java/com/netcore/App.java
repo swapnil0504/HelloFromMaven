5a48646bcbc6f9d2d3f7cb12251e9049edc45935
Swapnil Srivastav <swapnil.srivastav@netcore.co.in>
2016-12-30 12:02:15 +0530

    Initial Commit

---
+++ src/main/java/com/netcore/App.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.netcore;
+
+/**
+ * @author Swapnil Srivastav
+ *
+ */
+public class App {
+	
+	public String getMsg(){
+		return "Hi From Maven";
+	}
+
+}
---
+++ src/test/java/com/netcore/TestApp.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.netcore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Swapnil Srivastav
+ *
+ */
+public class TestApp {
+
+	@Test
+	public void test() {
+		assertEquals("Hi From Maven", new App().getMsg());
+	}
+
+}
