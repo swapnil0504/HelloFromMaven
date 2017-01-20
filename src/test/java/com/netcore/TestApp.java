21e574aae797a6041fd2d282707171998d1ff76b
swapnil0504 <swapnil3533@gmail.com>
2017-01-20 18:57:12 +0530

    Update TestApp.java

--- src/test/java/com/netcore/TestApp.java
+++ src/test/java/com/netcore/TestApp.java
@@ -1,21 +1,15 @@
-/**
- * 
- */
-package com.netcore;
 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * @author Swapnil Srivastav
- *
- */
-public class TestApp  {
+
+public class TestApp {
 
 	@Test
 	public void test() {
-		assertEquals("Hi From Maven", new App().getMsg());
+		assertEquals("Hi", new App().showMsg());
 	}
 
 }
